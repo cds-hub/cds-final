f30ad384bf826b98dde41cc7ba42a1c5b8d5ee5e
Marcos <comunidadesaberdo@gmail.com>
2019-11-29 22:01:44 -0200

    pagina de login e admin

--- src/app/cds/cds.module.ts
+++ src/app/cds/cds.module.ts
@@ -5,6 +5,8 @@ import { CursosComponent } from './cursos/';
 import { EventoComponent } from './evento';
 import { ContatoComponent } from './contato';
 import { InstituicaoComponent } from './instituicao';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 
@@ -14,7 +16,9 @@ import { InstituicaoComponent } from './instituicao';
     CursosComponent,
     EventoComponent,
     ContatoComponent,
-    InstituicaoComponent
+    InstituicaoComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     CommonModule
